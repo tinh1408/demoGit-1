ae935a1abe8e14ac4ea5a3e4aba6bef0628d2c5b
0935093371 <nguyenthimytam2010@gmail.com>
2018-09-26 20:30:35 +0700

    lan dau comit file lan sua sau

--- src/com/company.java
+++ src/com/company.java
@@ -1,7 +1,13 @@
 package com;
 
+import java.util.Scanner;
+
 public class company {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" nhap vao ten:");
+        String ten = scanner.nextLine();
+        System.out.println();
     }
 }
