46505a45457b637fe16b82a24c31eb4349b8439a
0935093371 <nguyenthimytam2010@gmail.com>
2018-09-26 20:41:39 +0700

    lan dau comit file lan sua sau

---
+++ src/com/baihoc.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.Scanner;
+
+public class baihoc {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        System.out.println(" nhap vao diem toan:");
+        int toan = scanner.nextInt();
+        System.out.println(" nhap vao diem ly:");
+        int ly = scanner.nextInt();
+        System.out.println(" nhap vao diem hoa:");
+        int hoa = scanner.nextInt();
+        int tong = toan + ly + hoa;
+        System.out.println(" tong diem:"+tong);
+
+}
+}
