21e45ca0591fad27027f837fb3ee796472d40cac
0935093371 <nguyenthimytam2010@gmail.com>
2018-09-28 21:03:54 +0700

    lan dau comit file lan sua sau

--- src/bien/diemTB.java
+++ src/bien/diemTB.java
@@ -10,9 +10,14 @@ public class diemTB {
         float diemLy = scanner.nextInt();
         System.out.println("nhap vao diem hoa:");
         float diemHoa = scanner.nextInt();
-        float diemTB =(diemHoa + diemLy + diemToan)/3;
-        System.out.println("nhap vao diem trung binh:" + diemTB);
-        float tong =(diemHoa + diemLy + diemToan);
-        System.out.println("nhap vao diem trung binh:" + tong);
+
+        if(((diemHoa >=0) && (diemHoa <=10))&&((diemLy >=0) && (diemLy <=10))&&((diemToan >=0) && (diemToan <=10))) {
+            float diemTB = (diemHoa + diemLy + diemToan) / 3;
+            System.out.println("nhap vao diem trung binh:" + diemTB);
+            float tong = (diemHoa + diemLy + diemToan);
+            System.out.println("nhap vao diem trung binh:" + tong);
+        }else {
+            System.out.println("loi");
+        }
     }
 }
