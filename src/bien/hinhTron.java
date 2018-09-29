c4da56babd0132eb9f1da375c248817aebe1264d
tinh1408 <tinhph@hotmail.com>
2018-09-29 22:36:54 +0700

    a Tính review code

--- src/bien/hinhTron.java
+++ src/bien/hinhTron.java
@@ -7,9 +7,9 @@ public class hinhTron {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao ban kinh:");
         int bankinh = scanner.nextInt();
-        double CV = bankinh * 2 * 3.14;
+        double CV = bankinh * 2 * 3.14; // 3.14 được sử dụng lại nhiều lần nên trường hợp này em nên khai báo hằng số cho 3.14 ví dụ: final int PI = 3.14;
         System.out.println("chu vi hinh tron :" + CV);
-        double DT = bankinh* bankinh *3.14;
+        double DT = bankinh* bankinh *3.14; // 3.14 được sử dụng lại nhiều lần nên trường hợp này em nên khai báo hằng số cho 3.14 ví dụ: final int PI = 3.14;
         System.out.println("chu vi hinh tron :" + DT);
     }
 }
