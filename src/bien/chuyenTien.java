7bcfb4f8c0b3e3b2978c20477b87be34acad040b
tinh1408 <tinhph@hotmail.com>
2018-09-29 22:33:32 +0700

    A tính review code

--- src/bien/chuyenTien.java
+++ src/bien/chuyenTien.java
@@ -6,7 +6,7 @@ public class chuyenTien {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao so USD:");
-        int USD = scanner.nextInt();
+        int USD = scanner.nextInt(); // em khai báo biến int cũng được tuy nhiên có thể khai báo biến float để phù hợp hơn ví dụ 20.5$
         int sotienVN =USD * 23000;
         System.out.println("tien USD:"+sotienVN);
     }
