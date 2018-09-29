2aea91e012e5dbfab60e4f22abcb2458a1047895
tinh1408 <tinhph@hotmail.com>
2018-09-29 22:35:12 +0700

    A Tính review code

--- src/bien/diemTB.java
+++ src/bien/diemTB.java
@@ -5,11 +5,11 @@ public class diemTB {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao diem toan:");
-        float diemToan = scanner.nextInt();
+        float diemToan = scanner.nextInt(); // Lưu ý nhé: khai báo biến float thì phải sử dụng nextFloat
         System.out.println("nhap vao diem ly:");
-        float diemLy = scanner.nextInt();
+        float diemLy = scanner.nextInt();  // tương tự
         System.out.println("nhap vao diem hoa:");
-        float diemHoa = scanner.nextInt();
+        float diemHoa = scanner.nextInt(); // tương tự
 
         if(((diemHoa >=0) && (diemHoa <=10))&&((diemLy >=0) && (diemLy <=10))&&((diemToan >=0) && (diemToan <=10))) {
             float diemTB = (diemHoa + diemLy + diemToan) / 3;
