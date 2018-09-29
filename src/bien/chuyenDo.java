af53121ce0dcf72dc0837db3baf610a88aa54bbf
tinh1408 <tinhph@hotmail.com>
2018-09-29 22:27:23 +0700

    anh Tính review code

--- src/bien/chuyenDo.java
+++ src/bien/chuyenDo.java
@@ -6,8 +6,10 @@ public class chuyenDo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao doF:");
-        float F = scanner.nextInt();
+        float F = scanner.nextInt(); // nếu anh nhập vào 12.5 thì chương trình của em sẽ lỗi vì ở đây em khai báo biến float nhưng lại sử dụng hàm nextInt
         float doC = 5* (F-32)/9;
         System.out.println("Do C la:" +doC);
+        
+        
     }
 }
