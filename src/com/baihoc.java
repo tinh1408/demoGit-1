5ff8b90ab96baca6cd1dca7594bade32e87c2bf2
tinh1408 <tinhph@hotmail.com>
2018-09-29 22:14:08 +0700

    anh tính góp ý

--- src/com/baihoc.java
+++ src/com/baihoc.java
@@ -13,6 +13,13 @@ public class baihoc {
         int hoa = scanner.nextInt();
         int tong = toan + ly + hoa;
         System.out.println(" tong diem:"+tong);
+        
+        /* anh tinh gop y
+        1. em nên khai báo biến trước khi sử dụng ví dụ
+        int toan;
+        toan = scanner.nextInt();
+        2. biến toan,ly,hoa khai báo int thì chỉ nhập được số nguyên trong khi đó điểm có thể là 5.9 hoặc 7.8 (em xem lại kiểu dữ liệu cho phù hợp nhé)
+       
 
     }
 }
