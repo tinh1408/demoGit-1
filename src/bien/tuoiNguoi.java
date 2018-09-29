0b8007344c28add5d53296dcc6b0323f15f7e944
tinh1408 <tinhph@hotmail.com>
2018-09-29 22:38:36 +0700

    a Tính review code

--- src/bien/tuoiNguoi.java
+++ src/bien/tuoiNguoi.java
@@ -7,7 +7,7 @@ public class tuoiNguoi {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao nam sinh:");
         int namsinh = scanner.nextInt();
-        int tuoi = 2018 - namsinh;
+        int tuoi = 2018 - namsinh;  // chương trình của em chỉ chạy đúng trong năm 2018 vậy giả sử qua năm 2019 hoặc 2020 anh phải sửa lại chương trình đúng không ? Vậy phải làm thế nào nhỉ ? em suy nghĩ cách làm thử nha
         System.out.println("tuoi cua mot nguoi:" + tuoi);
     }
 }
