dd5059696c8c18541129f3fdc580beef30542a22
0935093371 <43141581+0935093371@users.noreply.github.com>
2018-10-01 19:24:02 +0700

    Update hinhTron.java

--- src/bien/hinhTron.java
+++ src/bien/hinhTron.java
@@ -6,7 +6,7 @@ public class hinhTron {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao ban kinh:");
-        int bankinh = scanner.nextInt();
+        float bankinh = scanner.nextFloat();
         double CV = bankinh * 2 * 3.14;
         System.out.println("chu vi hinh tron :" + CV);
         double DT = bankinh* bankinh *3.14;
