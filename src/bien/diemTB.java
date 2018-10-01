6c4009975f31465598851acdeff2f0b19514737c
0935093371 <43141581+0935093371@users.noreply.github.com>
2018-10-01 19:20:59 +0700

    Update diemTB.java

--- src/bien/diemTB.java
+++ src/bien/diemTB.java
@@ -5,11 +5,11 @@ public class diemTB {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao diem toan:");
-        float diemToan = scanner.nextInt();
+        float diemToan = scanner.nextFloat();
         System.out.println("nhap vao diem ly:");
-        float diemLy = scanner.nextInt();
+        float diemLy = scanner.nextFloat();
         System.out.println("nhap vao diem hoa:");
-        float diemHoa = scanner.nextInt();
+        float diemHoa = scanner.nextFloat();
 
         if(((diemHoa >=0) && (diemHoa <=10))&&((diemLy >=0) && (diemLy <=10))&&((diemToan >=0) && (diemToan <=10))) {
             float diemTB = (diemHoa + diemLy + diemToan) / 3;
