9804929ef008cfce6ce59d15b748a44f7edda507
0935093371 <43141581+0935093371@users.noreply.github.com>
2018-10-01 19:22:35 +0700

    Update dienTichHCN.java

--- src/bien/dienTichHCN.java
+++ src/bien/dienTichHCN.java
@@ -6,9 +6,9 @@ public class dienTichHCN {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao chieu dai:");
-        float chieudai = scanner.nextInt();
+        float chieudai = scanner.nextFloat();
         System.out.println("nhap vao chieu rong:");
-        float chieurong = scanner.nextInt();
+        float chieurong = scanner.nextFloat();
         float dienTich = chieudai * chieurong ;
         System.out.println("dien tich:" +dienTich);
     }
