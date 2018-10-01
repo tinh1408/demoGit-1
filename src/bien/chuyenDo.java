27534a6b08e03c2621258839105083daa6f6faa8
0935093371 <43141581+0935093371@users.noreply.github.com>
2018-10-01 19:18:02 +0700

    Update chuyenDo.java

--- src/bien/chuyenDo.java
+++ src/bien/chuyenDo.java
@@ -6,7 +6,7 @@ public class chuyenDo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao doF:");
-        float F = scanner.nextInt();
+        float F = scanner.nextFloat();
         float doC = 5* (F-32)/9;
         System.out.println("Do C la:" +doC);
     }
