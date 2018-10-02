06a9feb3b8268f4948ec5f387098f175883d9fea
tinh1408 <tinhph@hotmail.com>
2018-10-02 09:30:30 +0700

    a Tính góp ý

--- src/bien/tuoiNguoi.java
+++ src/bien/tuoiNguoi.java
@@ -7,7 +7,7 @@ public class tuoiNguoi {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao nam sinh:");
         int namsinh = scanner.nextInt();
-        int tuoi = 2018 - namsinh;
+        int tuoi = 2018 - namsinh; // Năm sinh em cho nhập vào ví dụ như sang năm 2019 thì chương trình của em chạy không đúng nữa
         System.out.println("tuoi cua mot nguoi:" + tuoi);
     }
 }
