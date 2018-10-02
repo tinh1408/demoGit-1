7d1d06e41b1f5f2fb4fef03854cf3155efe6ac86
tinh1408 <tinhph@hotmail.com>
2018-10-02 09:31:41 +0700

    A Tính góp ý

--- src/bien/hinhTron.java
+++ src/bien/hinhTron.java
@@ -7,7 +7,7 @@ public class hinhTron {
         Scanner scanner = new Scanner(System.in);
         System.out.println("nhap vao ban kinh:");
         float bankinh = scanner.nextFloat();
-        double CV = bankinh * 2 * 3.14;
+        double CV = bankinh * 2 * 3.14;// 3.14 được sử dụng lại 2 lần và đây là số không thay đổi nên em set cho nó là hằng số em nha: Ví dụ: final int PI = 3.14;
         System.out.println("chu vi hinh tron :" + CV);
         double DT = bankinh* bankinh *3.14;
         System.out.println("chu vi hinh tron :" + DT);
